629e3626a341b553ba39af5cfc9397f80aecbd7f
Mark Haines <mjark@negativecurvature.net>
2016-01-15 17:44:30 +0000

    Replace the globals with a SynapseProxy struct, tidy up some of the error handling, copy accross all the headers

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 var (
 	synapseConfig = flag.String("synapse-config", "homeserver.yaml", "Path to a synapse config")
 	synapsePython = flag.String("synapse-python", "python", "Path to a python interpreter")
-	synapseUrlStr = flag.String("synapse-url", "http://localhost:18448", "Where synapse will be running")
+	synapseURLStr = flag.String("synapse-url", "http://localhost:18448", "Where synapse will be running")
 )
 
 var (
@@ -23,49 +23,52 @@ var (
 	listenKeyFile  = flag.String("key-file", "homeserver.key", "TLS Private Key")
 )
 
-var synapseUrl url.URL
+type SynapseProxy struct {
+	URL    url.URL
+	Client http.Client
+}
 
-var synapseClient http.Client
-
-func SynapseProxy(w http.ResponseWriter, req *http.Request) {
+func (p SynapseProxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	url := *req.URL
-	url.Scheme = synapseUrl.Scheme
-	url.Host = synapseUrl.Host
-	var synapseReq *http.Request
-	if sreq, err := http.NewRequest(req.Method, url.String(), req.Body); err != nil {
+	url.Scheme = p.URL.Scheme
+	url.Host = p.URL.Host
+	synapseReq, err := http.NewRequest(req.Method, url.String(), req.Body)
+	if err != nil {
 		w.WriteHeader(500)
 		fmt.Fprintln(w, "Error:", err)
-	} else {
-		synapseReq = sreq
+		return
 	}
 	synapseReq.ContentLength = req.ContentLength
-	synapseReq.Header["Content-Type"] = req.Header["Content-Type"]
-	synapseReq.Header["Content-Disposition"] = req.Header["Content-Dispostion"]
-	synapseReq.Header["Authorization"] = req.Header["Authorization"]
-	if resp, err := synapseClient.Do(synapseReq); err != nil {
+	for key, value := range req.Header {
+		synapseReq.Header[key] = value
+	}
+	resp, err := p.Client.Do(synapseReq)
+	if err != nil {
 		w.WriteHeader(500)
 		fmt.Fprintln(w, "Error:", err)
-	} else {
-		defer resp.Body.Close()
-
-		w.Header()["Content-Type"] = resp.Header["Content-Type"]
-		w.Header()["Content-Disposition"] = resp.Header["Content-Disposition"]
-		w.WriteHeader(resp.StatusCode)
-		io.Copy(w, resp.Body)
 	}
+	defer resp.Body.Close()
+
+	for key, value := range resp.Header {
+		w.Header()[key] = value
+	}
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
 }
 
 func main() {
 	flag.Parse()
 
-	if u, err := url.Parse(*synapseUrlStr); err != nil {
+	var synapseProxy SynapseProxy
+
+	if u, err := url.Parse(*synapseURLStr); err != nil {
 		panic(err)
 	} else {
-		synapseUrl = *u
+		synapseProxy.URL = *u
 	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", SynapseProxy)
+	mux.Handle("/", synapseProxy)
 	mux.HandleFunc("/_dendron/test", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintln(w, "test")
 	})
